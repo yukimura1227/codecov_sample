1bc9974b2e0af06a0f9a889c3e9a933f974eceeb
yukimura1227 <takamura1227@gmail.com>
2017-09-16 09:17:09 +0000

    sample

--- dummy_dir/spec/models/blog_spec.rb
+++ dummy_dir/spec/models/blog_spec.rb
@@ -1,5 +1,10 @@
 require 'rails_helper'
 
 RSpec.describe Blog, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  let(:blog) { create(:blog, title: 'hoge') }
+  describe '#title_with_embrace' do
+    it 'returns [#{title}]' do
+      expect(blog.title_with_embrace).to eq '[hoge]'
+    end
+  end
 end
